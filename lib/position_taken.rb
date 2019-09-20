02edc7b9a71d2e48553cc779e7a349f1043178c2
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-20 13:16:12 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board,index)
   if board[index]=="" || board[index]==" " || board[index]== nil
 return  false
-if board[index]=="X" || board[index]=="O"
+else
   return true 
   
 end 
